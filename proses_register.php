431577dc02e872fb34490eb22b2537e3f44dd58c
parhanhambali <parhanhambali13@gmail.com>
2021-05-21 20:15:47 +0700

    update proses register

--- proses_register.php
+++ proses_register.php
@@ -9,10 +9,24 @@
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $alamat = $_POST['alamat'];
-    $password = md5($_POST['password']);
+    $password = ($_POST['password']);
     $re_password = $_POST['re_password'];
 
-    mysqli_query($koneksi, "INSERT INTO user(level, nama, email, alamat, phone, password, status)
+    // digunakan untuk menghilangkan tampilan password di tampilan url
+    unset($_POST['password']);
+    unset($_POST['re_password']);
+
+    // digunakan agar data pada form tidak hilang pada saat user tidak lengkap mengisi data registrasi
+    $dataForm = http_build_query($_POST);
+
+    // digunakan untuk mengecek field yang terdapat pada form registrasi
+    if(empty($nama_lengkap) || empty($email) || empty($phone) || empty($alamat) || empty($password)){
+        header("location:".BASE_URL."index.php?page=register&notif=require&$dataForm");
+    }else{
+
+    //Koneksi ke tabel user
+        mysqli_query($koneksi, "INSERT INTO user(level, nama, email, alamat, phone, password, status)
                 VALUES('$level', '$nama_lengkap', '$email', '$alamat', '$phone', '$password', '$status')");
 
+    }
 ?>
--- register.php
+++ register.php
@@ -1,20 +1,38 @@
 <div id="container-user-akses">
     <form action="<?php echo BASE_URL."proses_register.php"; ?>" method="POST">
+    
+    
+    <?php 
+
+        // digunakan untuk menampilkan data pada form registrasi
+        $notif = isset($_GET['notif']) ? $_GET['notif'] : false;
+        $nama_lengkap = isset($_GET['nama_lengkap']) ? $_GET['nama_lengkap'] : false;
+        $email = isset($_GET['email']) ? $_GET['email'] : false;
+        $phone = isset($_GET['phone']) ? $_GET['phone'] : false;
+        $alamat = isset($_GET['alamat']) ? $_GET['alamat'] : false;
+        
+        // digunakan untuk menampilkan pernyataan bahwa pada form register telah diisi atau belum
+        if($notif == "require"){
+            echo "<div class='notif'>Lengkapi Data Registrasi Terlebih Dahulu</div>";
+        }
+    ?>
+
+
         <div class="element-form">
             <label>Nama Lengkap</label>
-            <span><input type="text" name="nama_lengkap"></span>
+            <span><input type="text" name="nama_lengkap" value="<?php echo $nama_lengkap; ?>"></span>
         </div>
         <div class="element-form">
             <label>Email</label>
-            <span><input type="text" name="email"></span>
+            <span><input type="text" name="email" value="<?php echo $email; ?>"></span>
         </div>
         <div class="element-form">
             <label for="">Nomor Telepon / Handphone</label>
-            <span><input type="text" name="phone"></span>
+            <span><input type="text" name="phone" value="<?php echo $phone; ?>"></span>
         </div>
         <div class="element-form">
             <label for="">Alamat</label>
-            <span><textarea name="alamat"></textarea></span>
+            <span><textarea name="alamat"><?php echo $alamat; ?></textarea></span>
         </div>
         <div class="element-form">
             <label for="">Password</label>
