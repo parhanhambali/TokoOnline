e69aa805f04f4af3c7f09917ae99ea366371c5a5
parhanhambali <parhanhambali13@gmail.com>
2021-05-22 23:06:45 +0700

    tambah form login

---
+++ login.php
@@ -0,0 +1,31 @@
+<div id="container-user-akses">
+    <div>
+        <form action="<?php echo BASE_URL."proses_login.php"; ?>" method="POST">
+        
+            <?php 
+
+                $notif = isset($_GET['notif']) ? $_GET['notif'] : false;
+
+                if($notif == "require"){
+                    echo "<div class='notif'>Email dan Password Tidak Sesuai</div>";
+                }
+            
+            ?>
+
+            <div class="element-form">
+                <label for="">Email</label>
+                <span><input type="text" name="email"></span>
+            </div>
+
+            <div class="element-form">
+                <label for="">Password</label>
+                <span><input type="password" name="password"></span>
+            </div>
+
+            <div>
+                <span><input type="submit" value="login"></span>
+            </div>
+            
+        </form>
+    </div>
+</div>
